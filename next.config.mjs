15ea9f3b9bc69e02aaab90e1d17579d202671c01
tyanpey <tyanpey@gmail.com>
2024-02-14 20:57:09 +0900

    app is now pwa ready.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})
+
+const nextConfig = withPWA({});
 
 export default nextConfig;
